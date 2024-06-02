5a1161849e3a550955fe4e764e80ad136ff0bf5a
Chris Backas <cbackas@mac.com>
2024-06-02 16:04:23 -0700

    Initial Commit

---
+++ JBC Station Monitor/JBC_Station_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JBC_Station_MonitorApp.swift
+//  JBC Station Monitor
+//
+//  Created by Chris Backas on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JBC_Station_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
